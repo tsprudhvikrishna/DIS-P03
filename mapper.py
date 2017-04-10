655fc3880e6470f4c87bf82e27c059ea71476fdc
tsprudhvikrishna <prudvikrishna78@gmail.com>
2017-04-10 17:01:14 -0500

    Created .gitignore file, code files, included all data, comments etc.

---
+++ mapper.py
@@ -0,0 +1,5 @@
+#This code file is to write mapper function on our project data set.
+#Mapper function will take data set as input to the function and performs sort/shuffle and gives intermediate key, value pairs as output.
+#Mapper input: color	director_name	num_critic_for_reviews	duration	director_facebook_likes	actor_3_facebook_likes	actor_2_name	actor_1_facebook_likes	gross	genres	actor_1_name	movie_title	num_voted_users	cast_total_facebook_likes	actor_3_name	facenumber_in_poster	plot_keywords	movie_imdb_link	num_user_for_reviews	language	country	content_rating	budget	title_year	actor_2_facebook_likes	imdb_score	aspect_ratio	movie_facebook_likes
+#Mapper Output: a.  actor_1_name  number_of_movies_count b.  director_name movie_title
+ 
---
+++ reducer.py
@@ -0,0 +1,5 @@
+#This code file is to write reducer function on our project data set.
+#Reducer function will take intermediate key, value pairs as input to the function and summarizes the data and produces key, value pairs as output.
+#Reducer input: a.  actor_1_name  number_of_movies_count b.  director_name movie_title
+#Reducer output: a.	 Will Smith 10 b. James Cameron Avatar
+
