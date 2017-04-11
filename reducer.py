605bfc8c330653384539d605be28f4eac9cba4ab
tsprudhvikrishna <prudvikrishna78@gmail.com>
2017-04-10 20:14:58 -0500

    Implemented reducer function for first MapReduce problem(movie actors and their movies count) and stored result in reducer_output.txt file

--- reducer.py
+++ reducer.py
@@ -3,3 +3,29 @@
 #Reducer input: a.  actor_1_name  number_of_movies_count b.  director_name movie_title
 #Reducer output: a.	 Will Smith 10 b. James Cameron Avatar
 
+sorted = open("mapper_sorted.txt","r")   
+results = open("reducer_output.txt", "w")   
+
+actorTotal = 0        
+oldMovie = None    
+lines = sorted.readlines()
+for line in lines:
+    datalist = line.strip().split("\t")
+    if len(datalist) != 2:  # if bad input line
+       continue             # ignore it
+
+    thisActor, thisMovie = datalist  # read into variables
+    if oldMovie and oldMovie != thisActor:        
+        results.write("{0}\t{1}\n".format(oldMovie, actorTotal))
+        oldMovie = thisActor;
+        actorTotal = 0
+
+    oldMovie = thisActor            
+    actorTotal += 1   
+
+if oldMovie != None: 
+    results.write("{0}\t{1}\n".format(oldMovie, actorTotal))
+
+sorted.close() 
+results.close() 
+
